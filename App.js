33f32f968a264cd497741596c8aecb85571020d5
Wanderley Filho <wanderley.filho@pier.digital>
2019-08-20 15:37:32 -0300

    project setup

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import AuthScreen from './src/screens/AuthScreen'
+import WelcomeScreen from './src/screens/WelcomeScreen'
+
+import MapScreen from './src/screens/MapScreen'
+import ReviewScreen from './src/screens/ReviewScreen'
+
+export default class App extends Component {
+
+  mainNavigator = createBottomTabNavigator({
+    welcome : { screen : WelcomeScreen},
+    auth : { screen : AuthScreen}
+  })
+
+  render() {
+      return (
+        <View style={styles.container}>
+          {this.mainNavigator}
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/screens/AuthScreen.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+
+export default class AuthScreen extends Component {
+
+    render() {
+        return(
+            <View>
+                <Text>
+                AuthScreen
+                </Text>
+            </View>
+        )
+    }
+}
