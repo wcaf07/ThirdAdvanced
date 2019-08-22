5e7d1ed6dd77a48c712af0534da858803cc48dd0
Wanderley Filho <wanderley.filho@pier.digital>
2019-08-22 15:28:31 -0300

    App loading until fb token is retrieved

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 import {Text, View} from 'react-native'
+import _ from 'loadash'
+import {AppLoading} from 'expo'
+import {AsyncStorage} from 'react-native'
 
 import Slides from '../components/Slides'
 
@@ -11,11 +14,26 @@ const SLIDE_DATA = [
 
 export default class WelcomeScreen extends Component {
 
+    state = { token : null}
+
+    componentDidMount = async() => {
+        let token = AsyncStorage.getItem('fb_login');
+        if (token) {
+            this.setState({ token : token})
+            this.props.navigation.navigate('map')
+        } else {
+            this.setState({ token : false})
+        }
+    }
+
     onCompleteSlide = () => {
         this.props.navigation.navigate('auth')
     }
 
     render() {
+        if (_.isNull(this.state.token)) {
+            return <AppLoading/>
+        }
         return(
             <View style={{flex : 1}}>
                 <Slides data={SLIDE_DATA} onComplete={this.onCompleteSlide}/>
